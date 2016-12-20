ec05a64d00d58fef1f93ac54dc735b1234ee8303
vladimirkush <vkushnirsky@gmail.com>
2016-12-20 04:34:07 +0200

    added layout for creating actions + changing views according to chosen action type

--- app/src/main/java/com/vladimirkush/geoaction/ActionCreate.java
+++ app/src/main/java/com/vladimirkush/geoaction/ActionCreate.java
@@ -3,15 +3,49 @@ package com.vladimirkush.geoaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import com.vladimirkush.geoaction.Utils.Constants;
+
+
 public class ActionCreate extends AppCompatActivity {
+
+    private Constants.ActionType actionType = Constants.ActionType.REMINDER;
+    // views
+    private RadioButton mRadioReminder;
+    private RadioButton mRadioSMS;
+    private RadioButton mRadioEmail;
+    private RadioButton mRadioEnterArea;
+    private RadioButton mRadioExitArea;
+    private LinearLayout mReminderLayout;
+    private LinearLayout mSMSLayout;
+    private LinearLayout mEmailLayout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_create);
+
+        // assign views
+        mRadioReminder = (RadioButton) findViewById(R.id.radio_reminder);
+        mRadioSMS = (RadioButton) findViewById(R.id.radio_sms);
+        mRadioEmail = (RadioButton) findViewById(R.id.radio_email);
+        mRadioEnterArea= (RadioButton) findViewById(R.id.radio_enter);
+        mRadioExitArea = (RadioButton) findViewById(R.id.radio_exit);
+        mReminderLayout = (LinearLayout) findViewById(R.id.reminder_container);
+        mSMSLayout = (LinearLayout) findViewById(R.id.sms_container);
+        mEmailLayout = (LinearLayout) findViewById(R.id.email_container);
+
+
+
+
+        mRadioReminder.setChecked(true);    // default checked radio
+        mRadioEnterArea.setChecked(true);
+        setViewByActionType(Constants.ActionType.REMINDER);
     }
 
     public void onRadioButtonClick(View view) {
@@ -23,22 +57,49 @@ public class ActionCreate extends AppCompatActivity {
             case R.id.radio_reminder:
                 if (checked)
                     // reminder
-
+                    setViewByActionType(Constants.ActionType.REMINDER);
                     break;
             case R.id.radio_sms:
                 if (checked)
                     // SMS
-
+                    setViewByActionType(Constants.ActionType.SMS);
                 break;
             case R.id.radio_email:
                 if (checked)
                     // email
-
+                    setViewByActionType(Constants.ActionType.EMAIL);
                 break;
         }
     }
 
     public void onLocationChooserClick(View view) {
         Toast.makeText(this, "clicked chose map", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onSaveActionClick(View view) {
+    }
+
+    /* setup views according to chosen type of action */
+    private void setViewByActionType(Constants.ActionType type){
+        actionType = type;
+        switch (type){
+
+            case EMAIL:
+                mSMSLayout.setVisibility(View.GONE);
+                mReminderLayout.setVisibility(View.GONE);
+                mEmailLayout.setVisibility(View.VISIBLE);
+                break;
+            case REMINDER:
+                mSMSLayout.setVisibility(View.GONE);
+                mReminderLayout.setVisibility(View.VISIBLE);
+                mEmailLayout.setVisibility(View.GONE);
+                break;
+
+            case SMS:
+                mSMSLayout.setVisibility(View.VISIBLE);
+                mReminderLayout.setVisibility(View.GONE);
+                mEmailLayout.setVisibility(View.GONE);
+                break;
+        }
     }
 }
--- app/src/main/java/com/vladimirkush/geoaction/Utils/Constants.java
+++ app/src/main/java/com/vladimirkush/geoaction/Utils/Constants.java
@@ -3,4 +3,8 @@ package com.vladimirkush.geoaction.Utils;
 
 public class Constants {
     public static final String LOGIN_IS_PERSISTENT_KEY = "Is login persistent";
+
+
+
+    public enum ActionType{REMINDER, SMS, EMAIL};
 }
